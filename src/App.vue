152f9c0c8de57e4e44cd988d3395a19f722862f9
Dmitriy <dmtr.hdj@gmail.com>
2023-11-21 20:43:51 +0300

    Добавлены: обнуление стилей, черновые варианты фона и верстки со стилями

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
+  <main>
+    <div class="container">
+      <div class="wrapper">
+        <div class="search">
+          <input type="text" class="search__bar" placeholder="поиск">
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
+const apiKey = 'e543924981f043e35272b2963f06c4a9'
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: 'Monserat', sans-serif;
+}
+
+.container {
+  background-image: url('./assets/img/cold_bg.jpg');
+  block-size: cover;
+  background-position: bottom;
+}
+
+.wrapper {
+  height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './assets/styles/normalize.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
